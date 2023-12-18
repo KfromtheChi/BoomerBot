dbe4cfe7a55b0255a6f0484c8431d7e0b4355aa4
Ahmadwali1 <walia3894@gmail.com>
2023-12-18 10:35:40 -0600

    create generativeAiconfig.js in config

---
+++ config/generativeAIConfig.js
@@ -0,0 +1,10 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+// ...
+
+const model = genAI.getGenerativeModel({ model: "MODEL_NAME"});
+
+// ...
