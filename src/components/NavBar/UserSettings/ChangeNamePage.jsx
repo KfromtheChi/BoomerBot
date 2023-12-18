51ed7f14075f11fa90d1c66256d2becb7f9f5ad7
Eric Erickrystalin Krystalin <krystalin.castillo@gmail.com>
2023-12-18 10:04:31 -0600

    Change Name Page

--- src/components/NavBar/UserSettings/ChangeNamePage.jsx
+++ src/components/NavBar/UserSettings/ChangeNamePage.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+import * as usersService from '../../../utilities/users-service';
+
+// name page functionality
 export default function ChangeNamePage() {
+    const [newName, setNewName] = useState('');
+
+    // new name change entry
+    const handleChange = (evt) => {
+        setNewName(evt.target.value);
+    }
+
+    // submit new name change
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        // setUser(newName);
+        // Call API to change name
+        // This is a placeholder, replace with your actual API call
+        // await api.changeName(newName);
+    }
+
+    // change name form
     return (
         <div>
-            <h1>Change Name Page</h1>
+            <h1>Change Name</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    New Name:
+                    <input type="text" value={newName} onChange={evt => setNewName(evt.target.value)} />
+                </label>
+                <input type="submit" value="Change Name" />
+            </form>
         </div>
     );
 };
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 
-// update user state defined in App from SignUpForm - setUser as a prop
+// update user state defined in App from LoginForm - setUser as a prop
 export default function LoginPage({ setUser }) {
   const [login, setLogin] = useState(true);
   const toggleForm = () => {
@@ -10,13 +10,13 @@ export default function LoginPage({ setUser }) {
   };
 
   return (
-    <main>
-      <h1>BoomerBot</h1>
+    <div className="login-container">
+      <header><h1>BoomerBot</h1></header>
       {login ? <LoginForm setUser={setUser} /> :  <SignUpForm setUser={setUser} />}
       <p>
         {login ? "Don't have an account? " : "Already have an account? "}
         <button onClick={toggleForm}>{login ? "Sign Up" : "Login"}</button>
       </p>
-    </main>
+    </div>
   );
 }
