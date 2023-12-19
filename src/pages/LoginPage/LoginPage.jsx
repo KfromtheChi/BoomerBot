967005b2936b883ad18cd0dae95ddf3fc1c50b64
Ahmadwali1 <walia3894@gmail.com>
2023-12-19 09:25:24 -0600

    add some classes to loginpage.jsx

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import './LoginPage.css'; // Import your custom styles for LoginPage
 
 // update user state defined in App from SignUpForm - setUser as a prop
 export default function LoginPage({ setUser }) {
@@ -10,13 +11,15 @@ export default function LoginPage({ setUser }) {
   };
 
   return (
-    <main>
-      <h1>BoomerBot</h1>
-      {login ? <LoginForm setUser={setUser} /> :  <SignUpForm setUser={setUser} />}
-      <p>
-        {login ? "Don't have an account? " : "Already have an account? "}
-        <button onClick={toggleForm}>{login ? "Sign Up" : "Login"}</button>
-      </p>
+    <main className="login-page">
+      <div className="login-container">
+        <h1 className="logo">BoomerBot</h1>
+        {login ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+        <p>
+          {login ? "Don't have an account? " : "Already have an account? "}
+          <button onClick={toggleForm}>{login ? "Sign Up" : "Login"}</button>
+        </p>
+      </div>
     </main>
   );
 }
