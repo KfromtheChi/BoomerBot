3dc271648c6503740ceb495f70808e397e60c9a6
adrimsaav <adrimsaav@gmail.com>
2023-12-20 18:06:54 -0500

    add to menu

--- src/components/NavBar/SettingsMenu.jsx
+++ src/components/NavBar/SettingsMenu.jsx
@@ -4,28 +4,26 @@ import AboutPage from './UserSettings/AboutPage/AboutPage';
 import ChangeNamePage from './UserSettings/ChangeNamePage';
 import ChangePasswordPage from './UserSettings/ChangePasswordPage';
 import DeleteAccountPage from './UserSettings/DeleteAccountPage';
+import './SettingsMenu.css'
 
 
 export default function SettingsMenu() {
-
   return (
     <div>
       <nav>
         <ul>
-            <ul>
-                <li>
-                  <Link to="/settings/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/settings/change-name">Change Name</Link>
-                </li>
-                <li>
-                  <Link to="/settings/change-password">Change Password</Link>
-                </li>
-                <li>
-                  <Link to="/settings/delete-account">Delete Account</Link>
-                </li>
-              </ul>
+          <li>
+            <Link to="/settings/about" className="menu-link">About</Link>
+          </li>
+          <li>
+            <Link to="/settings/change-name" className="menu-link">Change Name</Link>
+          </li>
+          <li>
+            <Link to="/settings/change-password" className="menu-link">Change Password</Link>
+          </li>
+          <li>
+            <Link to="/settings/delete-account" className="menu-link">Delete Account</Link>
+          </li>
         </ul>
       </nav>
     </div>
