1c64e194d6617e0d2e4c90bd720a727fbedbbbe7
KfromtheChi <krystalin.castillo@gmail.com>
2023-12-20 01:03:07 -0600

    About page complete

--- src/components/NavBar/UserSettings/AboutPage/AboutPage.jsx
+++ src/components/NavBar/UserSettings/AboutPage/AboutPage.jsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
             <div className="about-section">
                 <h1>ABOUT</h1>
                 <p>
-                    Just a group of developers-in-the-making from General Assembly's Software Engineering Immersive program who created this website to help seniors everywhere get used to the ever-changing world of technology. We hope you enjoy our site!
+                    Developers-in-the-making from General Assembly's Software Engineering Immersive program created this web application to help seniors everywhere get used to the ever-changing world of technology.  Ask it, snap it, and your AI response will be granted.  Cheers to learning anything and everythin.  We hope that you and your Boomers, whoever they may be, enjoy our site!
                 </p>
                 <h2 className="creators">CREATORS</h2>
                 <h3>Nial Attanayake</h3>
