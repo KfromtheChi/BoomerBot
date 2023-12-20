5a26eae85294f60b8362338bbefa9dd157ddc356
root <root@DESKTOP-PV5G1DG.>
2023-12-20 09:59:23 -0500

    Add prompt and safety

--- config/generativeAIConfig.js
+++ config/generativeAIConfig.js
@@ -1,33 +1,33 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const generationConfig = {
-  maxOutputTokens: 150,
+  maxOutputTokens: 248,
+  temperature: 0.4,
 }
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  }
+];
+
 
 // ...
-const geminiModel = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig });
+const geminiModel = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig, safetySettings });
 
-async function run() {
-  
-  const geminiModel = genAI.getGenerativeModel({ model: "gemini-pro" });
-  
-  // const prompt = "You are a baby boomer, you do not need to introduce yourself as such, however you must speak like a baby boomer at all times. You should also be using emojis incorrectly during your response, as a baby boomer would. Please respond to my input."
-  const prompt = "Write a story about a magic backpack."
-
-  const result = await geminiModel.generateContent(prompt);
-  const response = await result.response;
-  const text = await response.text();
-  console.log(text);
-}
-
-run();
 
 // ...
 module.exports = {
   genAI,
   geminiModel,
-  generationConfig
+  generationConfig,
+  safetySettings
 };
--- controllers/api/geminiController.js
+++ controllers/api/geminiController.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const genAI = require("../../config/generativeAIConfig");
+const { HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
+
+const generationConfig = {
+  maxOutputTokens: 248,
+  temperature: 0.4,
+}
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  }
+];
 
 async function askGemini(req, res) {
   const { prompt } = req.body;
@@ -8,7 +25,7 @@ async function askGemini(req, res) {
   try {
     // Use genAI to interact with the GoogleGenerativeAI model
     const model = genAI.geminiModel;
-    const result = await model.generateContent(prompt);
+    const result = await model.generateContent(`You are a chat bot named BoomerBot. You must act and respond as if you were a grumpy robot version of a baby boomer from the 1940s at all times. You should also be using emojis randomly, as a baby boomer would. Please respond to my input: ${prompt}`);
     const response =  result.response;
     const text = response.text();
     console.log(text);
@@ -24,4 +41,6 @@ async function askGemini(req, res) {
 
 module.exports = {
   askGemini,
+  safetySettings,
+  generationConfig,
 };
